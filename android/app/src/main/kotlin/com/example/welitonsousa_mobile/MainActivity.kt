4434edd8a0a30186a3f4cb8b410fc137b1dfeb36
Weliton Sousa <welitonubuntu@gmail.com>
2021-04-06 15:08:09 -0300

    config initial

---
+++ android/app/src/main/kotlin/com/example/welitonsousa_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.welitonsousa_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:welitonsousa_mobile/env.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: env.title,
+      debugShowCheckedModeBanner: env.showBanner,
+      home: Container(),
+    );
+  }
+}
