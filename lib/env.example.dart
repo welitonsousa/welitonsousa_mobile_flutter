6167960c60b5aa679a5c0d7ec1d90d6130cab02d
Weliton Sousa <welitonubuntu@gmail.com>
2021-04-19 17:36:33 -0300

    new env

--- lib/env.example.dart
+++ lib/env.example.dart
@@ -1,11 +1,5 @@
-// class _Env {
-//   static final bool _showBanner = true;
-//   static final String _title = "My blog";
-//   static final String _urlBase = "My url";
-
-//   bool get showBanner => _showBanner;
-//   String get title => _title;
-//   String get urlBase => _urlBase;
-// }
-
-// _Env env = _Env();
+class Env {
+  static final bool showBanner = true;
+  static final String urlBase = "";
+  static final String fireKey = "";
+}
